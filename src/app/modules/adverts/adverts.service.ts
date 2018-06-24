2792ef86acf84f5e74ba13936c044ce5bc4e3452
Honayme <honayme@gmail.com>
2018-06-24 15:08:31 +0200

    [Angular Advert] Add all the services to advert in angular

--- src/app/modules/adverts/adverts.service.ts
+++ src/app/modules/adverts/adverts.service.ts
@@ -10,7 +10,27 @@ export class AdvertsService {
   constructor(private http: HttpClient) {
   }
     getAll(): Observable<Adverts[]> {
-      //return this.http.get<Adverts[]>('/api/adverts/all'); Run build
-      return this.http.get<Adverts[]>('http://localhost:3000/api/adverts/all'); // ng serve
+      // return this.http.get<Adverts[]>('/api/adverts/all'); Run build
+      return this.http.get<Adverts[]>('http://localhost:3000/api/adverts/all');
+    }
+
+    getDetail(): Observable<Adverts[]> {
+      return this.http.get<Adverts[]>('http://localhost:3000/api/adverts/detail');
+    }
+
+    getUserAdvert(): Observable<Adverts[]> {
+      return this.http.get<Adverts[]>('http://localhost:3000/api/adverts/me');
+    }
+
+    createAdvert(data): Observable<Adverts[]> {
+      return this.http.post<Adverts[]>('http://localhost:3000/api/adverts/create', data);
+    }
+
+    updateAdvert(data): Observable<Adverts[]> {
+      return this.http.put<Adverts[]>('http://localhost:3000/api/adverts/update', data.id);
+    }
+
+    deleteAdvert(): Observable<Adverts[]> {
+      return this.http.delete<Adverts[]>('http://localhost:3000/api/adverts/delete');
     }
 }
