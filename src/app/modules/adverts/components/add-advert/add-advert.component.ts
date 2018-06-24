9fb4bf8884de6a0396fd95ff3022251e110f8ce4
Honayme <honayme@gmail.com>
2018-06-24 23:46:19 +0200

    [Angular + Add Advert] #Fix the correct error message for every fields

--- src/app/modules/adverts/components/add-advert/add-advert.component.ts
+++ src/app/modules/adverts/components/add-advert/add-advert.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Adverts } from '../../adverts';
 
-import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-add-advert',
@@ -10,18 +10,18 @@ import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 })
 export class AddAdvertComponent implements OnInit {
   advertForm: FormGroup;
-  advert: Adverts = new Adverts();
+  // advert: Adverts = new Adverts();
 
   constructor(private fb: FormBuilder ) {}
 
   // TODO  Respecter la struture du formulaire avec les validators
   ngOnInit(): void {
     this.advertForm = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(3)]],
+      title: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(40)]],
       picture: ['', Validators.required],
-      price: ['', Validators.required],
-      desc: ['', Validators.required],
-      zip: ['', Validators.required],
+      price: ['', [Validators.required, Validators.min(1)]],
+      desc: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(500)] ],
+      zip: ['', [Validators.required, Validators.min(1)]],
     });
   }
 
@@ -30,10 +30,11 @@ export class AddAdvertComponent implements OnInit {
     console.log('Saved: ' + JSON.stringify(this.advertForm.value));
   }
 
+  //TODO ne marche plus à cause de la photo en type file
   populateTestData(): void {
     this.advertForm.setValue({
       title: 'Vélo',
-      picture: 'Photo de Vélo',
+      picture: '',
       price: '250',
       desc: 'Un super vélo de montagne',
       zip: '13100'
